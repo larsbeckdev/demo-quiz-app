27962ac5f60af84fda3582dcb29fa0ddc25e657b
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-02 08:24:08 +0000

    Refactor Header component to simplify structure, remove unused code, and improve GitHub button accessibility

--- src/app/layout/default/Header.vue
+++ src/app/layout/default/Header.vue
@@ -1,33 +1,22 @@
-<script setup>
-import LogoPrimary from "@/components/logo/LogoPrimary.vue";
+<script setup lang="ts">
 import LogoSecondary from "@/components/logo/LogoSecondary.vue";
 import ThemeToggle from "@/features/theme-toggle/ThemeToggle.vue";
-import { NIcon } from "naive-ui";
-import { h } from "vue";
 import { Github } from "lucide-vue-next";
-
-function renderIcon() {
-  return h(NIcon, null, {
-    default: () => h(Github),
-  });
-}
 </script>
 
 <template>
-  <div class="logo-box flex align-center items-center gap-2">
-    <!-- <LogoPrimary class="h-full" /> -->
+  <div class="logo-box flex items-center gap-2">
     <LogoSecondary class="h-full" />
   </div>
+
   <n-space>
-    <n-button tertiary >
-      <n-icon><Github /></n-icon>
-      <p class="hidden sm:block">Github</p>
+    <n-button tertiary>
+      <n-icon>
+        <Github />
+      </n-icon>
+      <span class="hidden sm:block">GitHub</span>
     </n-button>
 
     <ThemeToggle />
   </n-space>
-
-  <!-- <div class="flex gap-2"></div> -->
 </template>
-
-<style scoped></style>
